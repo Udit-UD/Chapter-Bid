58a9e196edc497ac777c0248a16d399e62405489
Udit Gupta <udgupta33@gmail.com>
2023-08-27 22:40:36 +0530

    signup axios

--- client/src/Components/SignupForm.jsx
+++ client/src/Components/SignupForm.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "../api/axios";
+
+const RegisterURL = "/register";
 
 const CredForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userName, setUserName] = useState('');
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    try{
+      const res = await axios.post(RegisterURL, 
+        JSON.stringify({userName, email, password}),
+        {
+          headers: {"Content-Type": "application/json"},
+          withCredentials: true
+        }
+      );
+      console.log(res?.data);
+      setUserName('');
+      setPassword('');
+      setEmail('');
+
+
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+
+
   return (
     <>
       <div className="h-screen bg-login-background bg-cover bg-center bg-no-repeat backdrop-blur-md flex justify-center items-center">
@@ -22,36 +51,47 @@ const CredForm = () => {
           {/* INPUTS */}
 
           <div className="register-inputs flex-col h-1/3 w-5/6 justify-center items-center mx-10 my-14">
-            <input
-              className="reg-inputs w-full my-2 p-2 bg-white-300 border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500 border-2"
-              type="text"
-              required
-              name="userName"
-              placeholder="Your Name"
-            />
-            <input
-              className="reg-inputs w-full my-2 p-2 bg-white-300 border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500 border-2"
-              type="email"
-              required
-              name="email"
-              placeholder="Email Address"
-            />
-            <input
-              className="reg-inputs w-full my-2 p-2 bg-white-300 border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500 border-2"
-              type="password"
-              required
-              name="password"
-              placeholder="Password"
-            />
+            <form onSubmit={handleSubmit} method="get">
+              <input
+                className="reg-inputs w-full my-2 p-2 bg-white-300 border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500 border-2"
+                type="text"
+                required
+                name="userName"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+                placeholder="Your Name"
+              />
+              <input
+                className="reg-inputs w-full my-2 p-2 bg-white-300 border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500 border-2"
+                type="email"
+                required
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email Address"
+              />
+              <input
+                className="reg-inputs w-full my-2 p-2 bg-white-300 border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500 border-2"
+                type="password"
+                required
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+              />
 
-            <button class="click-btn rounded-xl bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500 hover:from-pink-500 hover:to-yellow-500 hover:shadow-lg transition-all block mx-auto my-8 p-3 px-16 text-white font-bold">
-              {" "}
-              Sign Up{" "}
-            </button>
-
+              <button
+                className="click-btn rounded-xl bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500 hover:from-pink-500 hover:to-yellow-500 hover:shadow-lg transition-all block mx-auto my-8 p-3 px-16 text-white font-bold"
+                type="submit"
+              >
+                {" "}
+                Sign Up{" "}
+              </button>
+            </form>
             <h2 className="register-secondary text-center">
               Already a user?{" "}
-              <Link to="/login"
+              <Link
+                to="/login"
                 className="login-link underline text-black hover:text-white hover:bg-blue-400"
               >
                 Login Now
